e5d2dd08f78e1802eb2e06b9702f4c53f72a4e74
ramirodoi <ramirodoi@gmail.com>
2015-06-11 18:23:00 -0300

    +Se agrega una pantalla con los integrantes del grupo.

--- Documentación/_build/html/searchindex.js
+++ Documentación/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Grupo3_Mensajer0&#8217;s documentation!"],objects:{},titleterms:{grupo3_mensajer0:0,document:0,welcom:0,indic:0,tabl:0}})
+Search.setIndex({envversion:42,terms:{index:0,search:0,modul:0,html:[],content:0,quienes_somo:[],page:0,nosotro:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Grupo3_Mensajer0&#8217;s documentation!"],objects:{},titleterms:{grupo3_mensajer0:0,document:0,welcom:0,indic:0,tabl:0}})
